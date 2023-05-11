49abbe78e7529db963be2ebd2257098a7a91451d
Fede Otálvares <fnotalvares@gmail.com>
2023-05-11 21:53:25 +0200

    Add get method

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import { getThings } from "./controllers/thingsController.js";
 
 export const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.get("/things", getThings);
